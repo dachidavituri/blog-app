73755e251a3b6281c18fd490e0c3b06097eeff4c
Dachi davituri <davituridachi33@gmail.com>
2024-11-15 19:37:00 +0400

    add tailwind and absolute pathes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css";
 
 function App() {
-  return <></>;
+  return <div>dada</div>;
 }
 
 export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(() => {
+  const rootPath = path.resolve(process.cwd());
+  const srcPath = `${rootPath}/src`;
+  const componentsPath = `${srcPath}/components`;
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "~": rootPath,
+        "@": srcPath,
+        "&": componentsPath,
+      },
+    },
+  };
+});
